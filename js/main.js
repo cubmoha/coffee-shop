29f344826c3f2bda0faa9e9f200f67a87907a167
Mohammed Mills <mohammedmills41@gmail.com>
2021-11-16 14:48:47 -0500

    new page

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//nav toggle - select button and links
+
+const toggleButton = document.querySelector("#toggle-button")
+const nav = document.querySelector("#nabar-links")
+
+toggleButton.addEventListener("click" , () => {
+    nav.classList.toggleButton('active')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// const toggleButton = documment.getElementsByClassName('toggle-button')[0]
+// const navbarLinks =  document.getElementsByClassName('navbar-links')[0]
+
+
+// toggleButton.addEventListener('click', () => {
+// navbarLinks.classList.toggle('active')
+// })
